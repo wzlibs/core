701697ba4f1b076970f59beac3ceb87c9c31f669
nampv <pvnam198@gmail.com>
2023-11-16 09:29:22 +0700

    update navigation

--- src/main/java/com/wzlibs/core/Navigation.kt
+++ src/main/java/com/wzlibs/core/Navigation.kt
@@ -10,17 +10,17 @@ class Navigation private constructor(
     private val fragment: Fragment,
     @IdRes private val containerViewId: Int,
     @NavigationType private val navigationType: Int = ADD_FRAGMENT,
-    private val tag: String? = null,
-    private val shouldAddToBackStack: Boolean = true,
-    private val backStackName: String? = null
+    private val fragmentTag: String? = null,
+    private val backStackName: String? = null,
+    private var shouldAddToBackStack: Boolean = true
 ) {
 
     private fun transaction() {
         val transaction = fragmentManager.beginTransaction()
         if (navigationType == ADD_FRAGMENT) {
-            transaction.add(containerViewId, fragment, tag)
+            transaction.add(containerViewId, fragment, fragmentTag)
         } else {
-            transaction.replace(containerViewId, fragment, tag)
+            transaction.replace(containerViewId, fragment, fragmentTag)
         }
         if (shouldAddToBackStack) {
             transaction.addToBackStack(backStackName)
@@ -37,13 +37,17 @@ class Navigation private constructor(
         private var navigationType: Int = ADD_FRAGMENT
         private var tag: String? = null
         private var backStackName: String? = null
-        private var shouldAddToBackStack: Boolean = true
+        private var shouldAddToBackStack: Boolean = false
 
         fun navigationType(@NavigationType navigationType: Int) =
             apply { this.navigationType = navigationType }
 
-        fun tag(tag: String?) = apply { this.tag = tag }
-        fun backStackName(backStackName: String?) = apply { this.backStackName = backStackName }
+        fun setFragmentTag(tag: String?) = apply { this.tag = tag }
+        fun addToBackStack(name: String?) = apply {
+            shouldAddToBackStack = true
+            backStackName = name
+        }
+
         fun shouldAddToBackStack(shouldAddToBackStack: Boolean) =
             apply { this.shouldAddToBackStack = shouldAddToBackStack }
 
@@ -53,8 +57,8 @@ class Navigation private constructor(
             containerViewId,
             navigationType,
             tag,
-            shouldAddToBackStack,
-            backStackName
+            backStackName,
+            shouldAddToBackStack
         ).apply { transaction() }
     }
 }
