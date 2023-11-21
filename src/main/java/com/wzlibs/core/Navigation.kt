53fca912cc88a3a352170d9b1d2ae8a1315f2a6c
nampv <pvnam198@gmail.com>
2023-11-21 11:10:27 +0700

    update transition

--- src/main/java/com/wzlibs/core/Navigation.kt
+++ src/main/java/com/wzlibs/core/Navigation.kt
@@ -5,40 +5,58 @@ import androidx.annotation.IntDef
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 
-class Navigation(
+class Navigation private constructor(
     private val fragmentManager: FragmentManager,
     private val fragment: Fragment,
-    @IdRes private val containerViewId: Int
+    @IdRes private val containerViewId: Int,
+    @NavigationType private val navigationType: Int = ADD_FRAGMENT,
+    private val fragmentTag: String? = null,
+    private val backStackName: String? = null,
+    private var shouldAddToBackStack: Boolean = true
 ) {
 
-    @NavigationType
-    private var navigationType: Int = ADD_FRAGMENT
-    private var tag: String? = null
-    private var backStackName: String? = null
-    private var shouldAddToBackStack: Boolean = false
-
-    fun navigationType(@NavigationType navigationType: Int) = apply {
-        this.navigationType = navigationType
-    }
-
-    fun setFragmentTag(tag: String?) = apply { this.tag = tag }
-
-    fun addToBackStack(name: String?) = apply {
-        shouldAddToBackStack = true
-        backStackName = name
-    }
-
-    fun transaction() {
+    private fun transaction() {
         val transaction = fragmentManager.beginTransaction()
         if (navigationType == ADD_FRAGMENT) {
-            transaction.add(containerViewId, fragment, tag)
+            transaction.add(containerViewId, fragment, fragmentTag)
         } else {
-            transaction.replace(containerViewId, fragment, tag)
+            transaction.replace(containerViewId, fragment, fragmentTag)
         }
         if (shouldAddToBackStack) {
             transaction.addToBackStack(backStackName)
         }
         transaction.commit()
+    }
+
+    class Builder(
+        private val fragmentManager: FragmentManager,
+        private val fragment: Fragment,
+        @IdRes private val containerViewId: Int
+    ) {
+        @NavigationType
+        private var navigationType: Int = ADD_FRAGMENT
+        private var tag: String? = null
+        private var backStackName: String? = null
+        private var shouldAddToBackStack: Boolean = false
+
+        fun navigationType(@NavigationType navigationType: Int) =
+            apply { this.navigationType = navigationType }
+
+        fun setFragmentTag(tag: String?) = apply { this.tag = tag }
+        fun addToBackStack(name: String?) = apply {
+            shouldAddToBackStack = true
+            backStackName = name
+        }
+
+        fun build() = Navigation(
+            fragmentManager,
+            fragment,
+            containerViewId,
+            navigationType,
+            tag,
+            backStackName,
+            shouldAddToBackStack
+        ).apply { transaction() }
     }
 }
 
